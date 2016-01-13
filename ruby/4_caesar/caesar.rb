06eb07545a15e12ed773718c6dfa662794fc890a
weberjavi <weberjavi@gmail.com>
2016-01-13 13:04:49 +0100

    terminada primera iteración

--- ruby/4_caesar/caesar.rb
+++ ruby/4_caesar/caesar.rb
@@ -1,10 +1,20 @@
 def solve_cipher(input)
 	#Convertimos la frase de entrada en un array en el que cada letra será un elemento
 	input = input.split("")
-	#Creamos un array que, tras convertir las letras del input a su equivalente ASCII (mediante el método .ord), resta uno a cada elemento.
-	#La función .map genera un nuevo array con las condiciones descritas dentro del {block}
-	ascii = input.map { |i| i.ord - 1 }
+	#Creamos un each loop que, tras convertir las letras del input a su equivalente ASCII (mediante el método .ord), resta uno a cada elemento.
+	#Posteriormente poblamos el array con el resutado y en caso de que el valor sea menor que 97 (equivalente a "a" en caracteres ASCII) lo convertimos en 122 ("z")
+	ascii = []
+	input.each do |i| 
+		i = i.ord - 1 
+		if i < 97
+			i = 122
+			ascii << i
+		else
+			ascii << i
+		end
+	end
 	#Creamos un array que convierte la numeración ASCII de entrada en el caracter correspondiente (a través del método .chr)
+	#La función .map genera un nuevo array con las condiciones descritas dentro del {block}
 	descifrado = ascii.map { |i| i.chr }
 	#La función devuelve el string creado a partir del array 
 	return descifrado.join("")
