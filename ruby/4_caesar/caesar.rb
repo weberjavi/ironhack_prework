e55ae507aef7160ae66df75beb99f88760a3179c
weberjavi <weberjavi@gmail.com>
2016-01-13 17:25:23 +0100

    Ejercicio finalizado

--- ruby/4_caesar/caesar.rb
+++ ruby/4_caesar/caesar.rb
@@ -1,3 +1,6 @@
+
+#ITERATION 1
+
 def solve_cipher(input)
 	#Convertimos la frase de entrada en un array en el que cada letra será un elemento
 	input = input.split("")
@@ -21,3 +24,81 @@ def solve_cipher(input)
 end	
 
 solve_cipher("ifmmp")
+
+
+
+#ITERATION 2
+
+def solve_cipher(input, shift)
+	#Convertimos la frase de entrada en un array en el que cada letra será un elemento
+	input = input.split("")
+	#Creamos un each loop que, tras convertir las letras del input a su equivalente ASCII (mediante el método .ord), resta uno a cada elemento.
+	#Posteriormente poblamos el array con el resutado y en caso de que el valor sea menor que 97 (equivalente a "a" en caracteres ASCII) lo convertimos en 122 ("z")
+	ascii = []
+	input.each do |i| 
+
+		i = i.ord + shift
+
+		if i == 32 + shift
+
+			i = " "
+			ascii << i
+
+		elsif i < 97
+
+			i = 122
+			ascii << i
+
+		else
+
+			ascii << i
+
+		end
+	end
+	#Creamos un array que convierte la numeración ASCII de entrada en el caracter correspondiente (a través del método .chr)
+	#La función .map genera un nuevo array con las condiciones descritas dentro del {block} y convertido a string con el método .join
+	descifrado = ascii.map { |i| i.chr }.join("")
+	#La función devuelve el string creado a partir del array 
+	return descifrado
+end	
+
+solve_cipher("p| uhdo qdph lv grqdog gxfn", -3)
+
+#ITERATION 3
+
+def solve_cipher(input, shift = -3)
+	#Convertimos la frase de entrada en un array en el que cada letra será un elemento
+	input = input.split("")
+	#Creamos un each loop que, tras convertir las letras del input a su equivalente ASCII (mediante el método .ord), resta uno a cada elemento.
+	#Posteriormente poblamos el array con el resutado y en caso de que el valor sea menor que 97 (equivalente a "a" en caracteres ASCII) lo convertimos en 122 ("z")
+	ascii = []
+	input.each do |i| 
+
+		i = i.ord + shift
+
+		if i == 32 + shift
+
+			i = " "
+			ascii << i
+
+		elsif i < 97
+
+			i = 122
+			ascii << i
+
+		else
+
+			ascii << i
+
+		end
+	end
+	#Creamos un array que convierte la numeración ASCII de entrada en el caracter correspondiente (a través del método .chr)
+	#La función .map genera un nuevo array con las condiciones descritas dentro del {block} y convertido a string con el método .join
+	descifrado = ascii.map { |i| i.chr }.join("")
+	#La función devuelve el string creado a partir del array 
+	return descifrado
+end	
+
+solve_cipher("p| uhdo qdph lv grqdog gxfn")
+
+
